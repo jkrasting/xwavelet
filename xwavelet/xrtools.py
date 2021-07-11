c85951f4c032f2e3dda6eb821c110fafd0cc7658
John Krasting <John.Krasting@noaa.gov>
2021-07-11 19:07:43 -0400

    Added stats function

--- xwavelet/xrtools.py
+++ xwavelet/xrtools.py
@@ -6,6 +6,30 @@ import numpy as np
 import cftime
 import warnings
 
+from scipy.stats import skew
+
+
+def detrend_array(arr, dim="time", deg=1):
+    """Detrend an Xarray DataArray
+
+    Parameters
+    ----------
+    arr : xarray.DataArray
+        Input data array
+    dim : str, optional
+        Name of time dimension, by default "time"
+    deg : int, optional
+        Number of polynomial coefficients for fit, by default 1 (linear)
+
+    Returns
+    -------
+    xarray.DataArray
+        Detrended data array
+    """
+    coeffs = arr.polyfit(dim=dim, deg=deg)
+    fitted = xr.polyval(arr[dim], coeffs.polyfit_coefficients)
+    return arr - fitted
+
 
 def power_spectrum(wave, dim="time"):
     """Convert wavelet ouput to a power spectrum
@@ -52,6 +76,28 @@ def infer_time_freq(arr, dim="time"):
     return result
 
 
+def timeseries_stats(arr):
+    """Calculate time series statistics
+
+    Parameters
+    ----------
+    arr : xarray.DataArray
+        Input data array
+
+    Returns
+    -------
+    dict
+    """
+    return {
+        "mean": float(arr.mean()),
+        "stddev": float(arr.std()),
+        "arrmin": float(arr.min()),
+        "arrmax": float(arr.max()),
+        "skewness": float(skew(arr.values)),
+        "range": float(arr.max().values - arr.min().values),
+    }
+
+
 def wavelet(
     arr,
     dim="time",
@@ -62,6 +108,7 @@ def wavelet(
     s0=None,
     mother="MORLET",
     scaled=False,
+    detrend=True,
 ):
     """xarray wrapper for Torrence and Campo wavelet analysis
 
@@ -87,12 +134,17 @@ def wavelet(
         Rescale the wavelet coefficents so that the double integral is
         the total energy (varaiance multiplied by total time).
         Default is False
+    detrend : boolean, optional
+        Linearly detrend the input dataset, by default is True
 
     Returns
     -------
     xarray.DataArray
         n-dim xarray of wavelet transform (period, time)
     """
+
+    # detrend the dataset
+    arr = detrend_array(arr, dim=dim) if detrend is True else arr
 
     # infer time frequency of input array
     dts = {"Y": 1.0, "M": 1.0 / 12.0, "D": 1.0 / 365.0, "unknown": 1.0}
@@ -118,19 +170,19 @@ def wavelet(
 
     # define in/out dimensions
     core_dims = [["time"]]
-    output_dims = [["period", "time"]]
+    output_dims = [["period", "time"], ["time"]]
 
     # set arguments
     kwargs = {"dt": dt, "pad": pad, "dj": dj, "s0": s0, "J1": j1, "mother": mother}
 
     # perform wavelet
-    result = xr.apply_ufunc(
+    result, coi = xr.apply_ufunc(
         _wrap_wavelet,
         arr,
         input_core_dims=core_dims,
         output_core_dims=output_dims,
         kwargs=kwargs,
-        vectorize=True,
+        vectorize=False,
     )
 
     # calculate period
@@ -155,7 +207,16 @@ def wavelet(
         scale = xr.DataArray(scale, dims=("period"))
         result = result * np.sqrt(dt / (c_delta * scale))
 
-    return result
+    # Convert to time series of spectral power in each period
+    power = (np.abs(result)) ** 2
+
+    dset_out = xr.Dataset()
+    dset_out["wave"] = result
+    dset_out["cone_of_influence"] = xr.DataArray(coi, dims=("time"))
+    dset_out["timeseries"] = arr
+    dset_out["power"] = power
+
+    return dset_out
 
 
 def _wrap_wavelet(arr, **kwargs):
@@ -172,4 +233,4 @@ def _wrap_wavelet(arr, **kwargs):
         Results of wavelet transform
     """
     wave, period, scale, coi = xw.wavelets.wavelet(arr, **kwargs)
-    return wave
+    return wave, coi
